9c5e6d3b17f677f2b36af1149e1d869f14a3dd9d
Vasyl Skorych <vasyl.skorych@gmail.com>
2026-08-11 18:43:34 +0200

    fix: Use surface gap as bond length in PWHertzMindlinLiquid

--- Models/ParticleWall/PWHertzMindlinLiquid/ModelPWHertzMindlinLiquid.cpp
+++ Models/ParticleWall/PWHertzMindlinLiquid/ModelPWHertzMindlinLiquid.cpp
@@ -1,5 +1,6 @@
-/* Copyright (c) 2013-2020, MUSEN Development Team. All rights reserved.
-   This file is part of MUSEN framework http://msolids.net/musen.
+/* Copyright (c) 2013-2020, MUSEN Development Team.
+   Copyright (c) 2026, DyssolTEC GmbH.
+   All rights reserved. This file is part of MUSEN framework https://github.com/msolids/musen.
    See LICENSE file for license and warranty information. */
 
 #include "ModelPWHertzMindlinLiquid.h"
@@ -32,7 +33,7 @@ void CModelPWHertzMindlinLiquid::CalculatePW(double _time, double _timeStep, siz
 	const double   rcLen  = rc.Length();
 	const CVector3 rcNorm = rc / rcLen;
 
-	const double bondLength = std::max(rcLen, minThickness);
+	const double bondLength = std::max(rcLen - partRadius, minThickness);
 
 	// normal overlap
 	const double normOverlap =  partRadius - rcLen;
